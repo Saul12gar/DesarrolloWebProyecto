2a46464bad3266d3912aa97ccdd573d29c0e392d
Saul12gar <oswaldo1822garcia@gmail.com>
2026-04-15 17:00:28 -0600

    Cambio final

--- server/server.js
+++ server/server.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "https://desarrollo-web-proyecto-xi.vercel.app",
+    ],
     credentials: true,
   }),
 );
